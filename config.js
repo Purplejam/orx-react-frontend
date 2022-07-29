dd508c087383811287d7292f13084d2d8c733d58
Administrator <Purplejamkiev@gmail.com>
2022-07-29 21:00:52 +0300

    env update

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
-export const endpoint = `https://orx-react.herokuapp.com/graphql`;
-export const prodEndpoint = `https://orx-react.herokuapp.com/graphql`;
+export const endpoint = `https://orx-react.herokuapp.com/api/graphql`;
+export const prodEndpoint = `https://orx-react.herokuapp.com/api/graphql`;
 export const perPage = 4;
